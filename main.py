58b7f89d17e1f877103be2ad4bca57b6763a5dfa
mohsinhassaan <mohsinhassaan@gmail.com>
2019-12-14 13:41:11 +0500

    added commandline args, changed snake class to be independent of screen size and added passing through walls

--- main.py
+++ main.py
@@ -1,14 +1,80 @@
 import pygame, sys
+from sys import argv
 from pygame.locals import *
 from random import randint
 from collections import deque
 
 
+def main():
+    if len(argv) > 4 or len(argv) == 2 and (argv[1] == "--help" or argv[1] == "-h"):
+        print(f"Usage: {argv[0]} [width] [height] [pieces]")
+
+    global screen, w, h, pieces, piece_size
+
+    w, h = (int(argv[1]), int(argv[2])) if len(argv) >= 3 else (700, 700)
+    pieces = int(argv[3]) if len(argv) == 4 else 20
+    screen = pygame.display.set_mode((w, h))
+    piece_size = w / pieces
+    s = snake(pieces, int((pieces / w) * h))
+    a = generate_apple(s)
+
+    direction = "r"
+
+    screen.fill([64, 64, 64])
+    draw_snake(s)
+    draw_apple(a)
+    pygame.display.update()
+
+    paused = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN and event.key == K_SPACE:
+                break
+        else:
+            continue
+        break
+
+    while True:
+        lastdir = direction
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if not paused:
+                    if event.key == K_LEFT and lastdir != "r":
+                        direction = "l"
+                    elif event.key == K_RIGHT and lastdir != "l":
+                        direction = "r"
+                    elif event.key == K_DOWN and lastdir != "u":
+                        direction = "d"
+                    elif event.key == K_UP and lastdir != "d":
+                        direction = "u"
+                if event.key == K_ESCAPE:
+                    sys.exit()
+                elif event.key == K_r:
+                    s = snake(pieces, int((pieces / w) * h))
+                    a = generate_apple(s)
+                    direction = "r"
+                elif event.key == K_p:
+                    paused = True if paused == False else False
+        if not paused:
+            if s.alive:
+                eaten = s.move(direction, a)
+                if eaten:
+                    a = generate_apple(s)
+                screen.fill([64, 64, 64])
+                draw_snake(s)
+                draw_apple(a)
+            else:
+                gameover()
+
+            pygame.display.update()
+        pygame.time.delay(100)
+
+
 class snake(object):
-    def __init__(self, h: int, w: int, piece_size: int):
+    def __init__(self, h: int, w: int):
         self.height = h
         self.width = w
-        self.PIECE_SIZE = piece_size
         self.pieces = deque([snake_bit((0, 0))])
         self.alive = True
 
@@ -16,17 +82,20 @@ class snake(object):
         head = self.pieces[-1]
 
         if dir == "r":
-            pos = (head.pos[0] + self.PIECE_SIZE, head.pos[1])
+            pos = (head.pos[0] + 1, head.pos[1])
         elif dir == "l":
-            pos = (head.pos[0] - self.PIECE_SIZE, head.pos[1])
+            pos = (head.pos[0] - 1, head.pos[1])
         elif dir == "u":
-            pos = (head.pos[0], head.pos[1] - self.PIECE_SIZE)
+            pos = (head.pos[0], head.pos[1] - 1)
         elif dir == "d":
-            pos = (head.pos[0], head.pos[1] + self.PIECE_SIZE)
+            pos = (head.pos[0], head.pos[1] + 1)
 
         eating = True if pos == ap.pos else False
 
         if self.__valid_pos(pos, eating):
+            collision = self.__wall_collision(pos)
+            pos = collision if collision is not None else pos
+
             if eating:
                 self.pieces.append(snake_bit(pos))
                 return True
@@ -37,21 +106,25 @@ class snake(object):
         else:
             self.alive = False
 
+    def __wall_collision(self, pos: tuple):
+        if pos[0] < 0:
+            return (self.width - 1, pos[1])
+        elif pos[0] + 1 > self.width:
+            return (0, pos[1])
+        elif pos[1] < 0:
+            return (pos[0], self.height - 1)
+        elif pos[1] + 1 > self.height:
+            return (pos[0], 0)
+        else:
+            return None
+
     def __valid_pos(self, pos: tuple, eating: bool):
-        start = 1 if eating else 0
+        start = 2 if eating else 1
 
         for piece in list(self.pieces)[start:]:
             if piece.pos == pos:
                 return False
-
-        return (
-            True
-            if pos[0] >= 0
-            and pos[0] + self.PIECE_SIZE <= self.width
-            and pos[1] >= 0
-            and pos[1] + self.PIECE_SIZE <= self.width
-            else False
-        )
+        return True
 
 
 class snake_bit(object):
@@ -67,28 +140,49 @@ class apple(object):
 pygame.init()
 
 
-def draw_snake():
+def draw_snake(s: snake):
     for piece in s.pieces:
         pygame.draw.rect(
-            screen, (255, 255, 255), pygame.Rect(piece.pos, (piece_size, piece_size))
+            screen,
+            (255, 255, 255),
+            pygame.Rect(
+                (piece.pos[0] * piece_size, piece.pos[1] * piece_size),
+                (piece_size, piece_size),
+            ),
         )
         pygame.draw.rect(
-            screen, (0, 0, 0), pygame.Rect(piece.pos, (piece_size, piece_size)), 1
+            screen,
+            (0, 0, 0),
+            pygame.Rect(
+                (piece.pos[0] * piece_size, piece.pos[1] * piece_size),
+                (piece_size, piece_size),
+            ),
+            1,
         )
 
 
-def draw_apple():
-    pygame.draw.rect(screen, (200, 0, 0), pygame.Rect(a.pos, (piece_size, piece_size)))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(a.pos, (piece_size, piece_size)), 1)
+def draw_apple(a: apple):
+    pygame.draw.rect(
+        screen,
+        (200, 0, 0),
+        pygame.Rect(
+            (a.pos[0] * piece_size, a.pos[1] * piece_size), (piece_size, piece_size)
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        (0, 0, 0),
+        pygame.Rect(
+            (a.pos[0] * piece_size, a.pos[1] * piece_size), (piece_size, piece_size)
+        ),
+        1,
+    )
 
 
-def generate_apple():
+def generate_apple(s: snake):
     pos = None
     while True:
-        pos = (
-            randint(0, w // piece_size - 1) * piece_size,
-            randint(0, h // piece_size - 1) * piece_size,
-        )
+        pos = (randint(0, w // piece_size - 1), randint(0, h // piece_size - 1))
         valid = True
         for piece in s.pieces:
             if piece.pos == pos:
@@ -108,60 +202,5 @@ def gameover():
     screen.blit(text, text_rect)
 
 
-w, h = 800, 800
-screen = pygame.display.set_mode((w, h))
-piece_size = w / 20
-s = snake(w, h, piece_size)
-a = generate_apple()
-
-direction = "r"
-
-screen.fill([64, 64, 64])
-draw_snake()
-draw_apple()
-pygame.display.update()
-
-paused = False
-
-while True:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN and event.key == K_SPACE:
-            break
-    else:
-        continue
-    break
-
-while True:
-    lastdir = direction
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if not paused:
-                if event.key == K_LEFT and lastdir != "r":
-                    direction = "l"
-                elif event.key == K_RIGHT and lastdir != "l":
-                    direction = "r"
-                elif event.key == K_DOWN and lastdir != "u":
-                    direction = "d"
-                elif event.key == K_UP and lastdir != "d":
-                    direction = "u"
-            if event.key == K_ESCAPE:
-                sys.exit()
-            elif event.key == K_r:
-                s = snake(h, w, piece_size)
-                a = generate_apple()
-                direction = "r"
-            elif event.key == K_p:
-                paused = True if paused == False else False
-    if not paused:
-        if s.alive:
-            eaten = s.move(direction, a)
-            if eaten:
-                a = generate_apple()
-            screen.fill([64, 64, 64])
-            draw_snake()
-            draw_apple()
-        else:
-            gameover()
-
-        pygame.display.update()
-    pygame.time.delay(100)
+if __name__ == "__main__":
+    main()
