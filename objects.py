d2deec948e1941e9c71a1cabdeead55aa676b6a2
mohsinhassaan <mohsinhassaan@gmail.com>
2019-12-16 18:12:52 +0500

    stricter type checks

--- objects.py
+++ objects.py
@@ -1,39 +1,45 @@
 from collections import deque
+from typing import *
+
+
+class apple(object):
+    def __init__(self, pos):
+        self.pos: Tuple[int, int] = pos
 
 
 class snake(object):
     def __init__(self, w: int, h: int):
-        self.width = w
-        self.height = h
-        self.pieces = deque([snake_bit((0, 0))])
-        self.alive = True
+        self.width: int = w
+        self.height: int = h
+        self.pieces: Deque[snake.__snake_bit] = deque([snake.__snake_bit((0, 0))])
+        self.alive: bool = True
 
-    def move(self, dir, ap):
-        head = self.pieces[-1]
+    def move(self, dir: str, ap: apple):
+        head: snake.__snake_bit = self.pieces[-1]
 
         if dir == "r":
-            pos = (head.pos[0] + 1, head.pos[1])
+            pos: Tuple[int, int] = (head.pos[0] + 1, head.pos[1])
         elif dir == "l":
-            pos = (head.pos[0] - 1, head.pos[1])
+            pos: Tuple[int, int] = (head.pos[0] - 1, head.pos[1])
         elif dir == "u":
-            pos = (head.pos[0], head.pos[1] - 1)
+            pos: Tuple[int, int] = (head.pos[0], head.pos[1] - 1)
         elif dir == "d":
-            pos = (head.pos[0], head.pos[1] + 1)
+            pos: Tuple[int, int] = (head.pos[0], head.pos[1] + 1)
 
-        pos = self.__wall_collision(pos)
+        pos: Tuple[int, int] = self.__wall_collision(pos)
 
-        eating = True if pos == ap.pos else False
+        eating: bool = True if pos == ap.pos else False
 
         if self.__valid_pos(pos, eating):
             if eating:
-                self.pieces.append(snake_bit(pos))
+                self.pieces.append(snake.__snake_bit(pos))
                 return True
             else:
                 self.pieces.popleft()
-                self.pieces.append(snake_bit(pos))
+                self.pieces.append(snake.__snake_bit(pos))
                 return False
         else:
-            self.alive = False
+            self.alive: bool = False
 
     def __wall_collision(self, pos: tuple):
         if pos[0] < 0:
@@ -47,23 +53,15 @@ class snake(object):
         else:
             return pos
 
-    def __valid_pos(self, pos: tuple, eating: bool):
-        start = 1 if eating else 0
-
+    def __valid_pos(self, pos: Tuple[int, int], eating: bool):
         for piece in self.pieces:
             if piece.pos == pos:
                 return False
         return True
 
+    class __snake_bit(object):
+        def __init__(self, pos: Tuple[int, int]):
+            self.pos: Tuple[int, int] = pos
 
-class snake_bit(object):
-    def __init__(self, pos):
-        self.pos = pos
-
-    def __str__(self):
-        return str(self.pos)
-
-
-class apple(object):
-    def __init__(self, pos):
-        self.pos = pos
+        def __str__(self):
+            return str(self.pos)
